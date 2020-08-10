6867c9c9f420c66cd7caff97c92e16099d63e09e
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-08-10 11:13:35 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Ramesh Kumar");
+$("#policyNumber").text("PME7347005");
+$("#startDate").text("10-Aug-2020");
+$("#endDate").text("11-Aug-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB08CB3680");
+$("#make").text("BAJAJ");
+$("#model").text("PLATINA 100 CC ");
+$("#vehicalSubType").text("STD");
+$("#engineNo").text("DZUBUF67106");
+$("#chassis").text("MD2DDDZZZUPF86994");
